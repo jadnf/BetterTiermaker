b37e544f17972837ebd0ec346f362b5a5b85c214
JACE062 <123290590+JACE062@users.noreply.github.com>
2026-02-04 09:52:45 -0700

    Tierlist Page Changes

---
+++ front-end/src/TierlistPage.jsx
@@ -0,0 +1,10 @@
+
+export default function TierlistPage() {
+    return (
+        <div>
+            <h2>This is the Tierlist Page. wow, much tier, very list</h2>
+            <p>This is where the tierlist will go, we need to implement Dndcontext on the tierlist container, then we need to create the tier components that implement Droppable, and the tieritem component that implements Draggable</p>
+        </div>
+    )
+}
+
