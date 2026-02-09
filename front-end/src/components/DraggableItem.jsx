58192fd821f17117e876de1205ec47809df813fb
JACE062 <123290590+JACE062@users.noreply.github.com>
2026-02-09 09:21:18 -0700

    DraggableItem Update

--- front-end/src/components/DraggableItem.jsx
+++ front-end/src/components/DraggableItem.jsx
@@ -1,12 +1,17 @@
 import { useDraggable } from "@dnd-kit/core";
+import {CSS} from "@dnd-kit/utilities";
 
 export default  function DraggableItem(props) {
-    const {attributes, listeners, setNodeRef} = useDraggable({
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
         id : props.id
     });
 
+    const style = {
+        transform: CSS.Translate.toString(transform),
+    };
+
     return (
-        <div className="draggable-item" ref={setNodeRef} {...listeners} {...attributes}>
+        <div className="draggable-item" style={style} ref={setNodeRef} {...listeners} {...attributes}>
             <span>{props.id}</span>
         </div>
     );
